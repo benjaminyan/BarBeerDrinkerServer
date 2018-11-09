b0f1b7c273676b679f17d26525a28086b38a02eb
Eric Giovannini <eric.giovannini@rutgers.edu>
2018-11-09 17:31:58 -0500

    cross-origin request on get all drinkers

--- server/src/main/java/com/ericandben/cs336/backendapp/drinker/DrinkerController.java
+++ server/src/main/java/com/ericandben/cs336/backendapp/drinker/DrinkerController.java
@@ -3,6 +3,7 @@ package com.ericandben.cs336.backendapp.drinker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +26,7 @@ public class DrinkerController {
 		return "Saved";
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<Drinker> getAllDrinkers() {
 		// This returns a JSON or XML with the users
